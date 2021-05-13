07c45ff5f4ea56443d8b5827db995d3e48e04a43
Olga <25307442+comm0nsense@users.noreply.github.com>
2021-05-14 00:36:36 +0300

    Добавит новый компонент-заглушку, если в базе нет фильмов

--- src/main.js
+++ src/main.js
@@ -8,13 +8,14 @@ import StatisticsView from './view/statistics';
 import FilmCardView from './view/film-card';
 import ShowMoreBtnView from './view/show-more-btn';
 import FilmPopupView from './view/film-popup';
+import EmptyListView from './view/empty-list';
 import { generateComments, generateArrayOfCommentsIds, generateFilms} from './mock/film';
 import { generateFilter } from './mock/filter';
 import { render, sortByMostCommented, sortByRating} from './util';
 import { RenderPosition, ExtraListTitles } from './const';
 
 
-const FILM_COUNT = 12;
+const FILM_COUNT = 0;
 const FILM_COUNT_EXTRA_LIST = 2;
 const FILM_COUNT_PER_STEP = 5;
 
@@ -36,15 +37,14 @@ const siteBodyElement = document.querySelector('body');
 const siteMainElement = document.querySelector('.main');
 
 render(siteMainElement, new FilterView(filters).getElement(), RenderPosition.BEFOREEND);
-render(siteMainElement, new SortingView().getElement(), RenderPosition.BEFOREEND);
 render(siteMainElement, new FilmsBoardView().getElement(), RenderPosition.BEFOREEND);
 
 const siteFilmsBoardElement = document.querySelector('.films');
 
-const mainListComponent = new MainListView();
-render(siteFilmsBoardElement, mainListComponent.getElement(), RenderPosition.BEFOREEND);
-const siteMainListElement = document.querySelector('.films-list');
-const mainListContainer = siteMainListElement.querySelector('.films-list__container');
+// const mainListComponent = new MainListView();
+// render(siteFilmsBoardElement, mainListComponent.getElement(), RenderPosition.BEFOREEND);
+// const siteMainListElement = document.querySelector('.films-list');
+// const mainListContainer = siteMainListElement.querySelector('.films-list__container');
 
 const renderFilmCard = (container, film) => {
   const filmCardComponent = new FilmCardView(film);
@@ -88,47 +88,56 @@ const renderFilmPopup = (film) => {
   document.addEventListener('keydown', onEscKeyDown);
 };
 
-for (let i = 0; i < Math.min(films.length, FILM_COUNT_PER_STEP); i++) {
-  renderFilmCard(mainListContainer, films[i]);
+if (!films.length ) {
+  render(siteFilmsBoardElement, new EmptyListView().getElement(), RenderPosition.BEFOREEND);
+} else {
+  render(siteMainElement, new SortingView().getElement(), RenderPosition.BEFOREEND);
+  const mainListComponent = new MainListView();
+  render(siteFilmsBoardElement, mainListComponent.getElement(), RenderPosition.BEFOREEND);
+  const siteMainListElement = document.querySelector('.films-list');
+  const mainListContainer = siteMainListElement.querySelector('.films-list__container');
+
+  for (let i = 0; i < Math.min(films.length, FILM_COUNT_PER_STEP); i++) {
+    renderFilmCard(mainListContainer, films[i]);
+  }
+  if (films.length > FILM_COUNT_PER_STEP) {
+    let renderedFilmCount = FILM_COUNT_PER_STEP;
+
+    render(siteMainListElement, new ShowMoreBtnView().getElement(), RenderPosition.BEFOREEND);
+
+    const showMoreBtn = siteMainListElement.querySelector('.films-list__show-more');
+
+    showMoreBtn.addEventListener('click', (evt) => {
+      evt.preventDefault();
+      films
+        .slice(renderedFilmCount, renderedFilmCount + FILM_COUNT_PER_STEP)
+        .forEach((film) => renderFilmCard(mainListContainer, film));
+
+      renderedFilmCount += FILM_COUNT_PER_STEP;
+
+      if (renderedFilmCount >= films.length) {
+        showMoreBtn.remove();
+      }
+    });
+  }
+
+  const topRatedListComponent = new ExtraListView(ExtraListTitles.TOP_RATED);
+  render(siteFilmsBoardElement, topRatedListComponent.getElement(), RenderPosition.BEFOREEND);
+  const topRatedListContainer = topRatedListComponent.getElement().querySelector('.films-list__container');
+  const topRatedFilms = sortByRating(films);
+  topRatedFilms
+    .slice(0, FILM_COUNT_EXTRA_LIST)
+    .forEach((film) => renderFilmCard(topRatedListContainer, film));
+
+
+  const mostCommentedListComponent = new ExtraListView(ExtraListTitles.MOST_COMMENTED);
+  render(siteFilmsBoardElement, mostCommentedListComponent.getElement(), RenderPosition.BEFOREEND);
+  const mostCommentedListContainer = mostCommentedListComponent.getElement().querySelector('.films-list__container');
+  const mostCommentedFilms = sortByMostCommented(films);
+  mostCommentedFilms
+    .slice(0, FILM_COUNT_EXTRA_LIST)
+    .forEach((film) => renderFilmCard(mostCommentedListContainer, film));
 }
-
-if (films.length > FILM_COUNT_PER_STEP) {
-  let renderedFilmCount = FILM_COUNT_PER_STEP;
-
-  render(siteMainListElement, new ShowMoreBtnView().getElement(), RenderPosition.BEFOREEND);
-
-  const showMoreBtn = siteMainListElement.querySelector('.films-list__show-more');
-
-  showMoreBtn.addEventListener('click', (evt) => {
-    evt.preventDefault();
-    films
-      .slice(renderedFilmCount, renderedFilmCount + FILM_COUNT_PER_STEP)
-      .forEach((film) => renderFilmCard(mainListContainer, film));
-
-    renderedFilmCount += FILM_COUNT_PER_STEP;
-
-    if (renderedFilmCount >= films.length) {
-      showMoreBtn.remove();
-    }
-  });
-}
-
-const topRatedListComponent = new ExtraListView(ExtraListTitles.TOP_RATED);
-render(siteFilmsBoardElement, topRatedListComponent.getElement(), RenderPosition.BEFOREEND);
-const topRatedListContainer = topRatedListComponent.getElement().querySelector('.films-list__container');
-const topRatedFilms = sortByRating(films);
-topRatedFilms
-  .slice(0, FILM_COUNT_EXTRA_LIST)
-  .forEach((film) => renderFilmCard(topRatedListContainer, film));
-
-
-const mostCommentedListComponent = new ExtraListView(ExtraListTitles.MOST_COMMENTED);
-render(siteFilmsBoardElement, mostCommentedListComponent.getElement(), RenderPosition.BEFOREEND);
-const mostCommentedListContainer = mostCommentedListComponent.getElement().querySelector('.films-list__container');
-const mostCommentedFilms = sortByMostCommented(films);
-mostCommentedFilms
-  .slice(0, FILM_COUNT_EXTRA_LIST)
-  .forEach((film) => renderFilmCard(mostCommentedListContainer, film));
 
 const siteFooterStatisticsElement = document.querySelector('.footer__statistics');
 render(siteFooterStatisticsElement, new StatisticsView(films.length).getElement(), RenderPosition.BEFOREEND);
